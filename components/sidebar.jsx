b3d335311bfebf2080a79e76e4aa0a4158887962
Guillaume Bielli <guillaume.bielli@gmail.com>
2026-03-23 10:00:19 +0100

    export gtm

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -7,9 +7,9 @@ import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
   const menuItems = [
-    { href: "/ga4", label: "Générateur GA4" },
+    // { href: "/ga4", label: "Générateur GA4" },
     { href: "/variables", label: "Générateur variables" },
-    { href: "/events", label: "Générateur events" },
+    { href: "/events", label: "Générateur de déclencheurs" },
     { href: "/tracking-plan", label: "Tracking Plan" },
   ];
 
@@ -21,7 +21,10 @@ const Sidebar = () => {
     <div className="hidden border-r border-sidebar-border bg-sidebar md:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
         <div className="flex h-18 items-center border-b border-sidebar-border px-4 lg:h-[75px] lg:px-6 py-4">
-          <Link href="/events" className="flex items-center gap-2 font-semibold">
+          <Link
+            href="/events"
+            className="flex items-center gap-2 font-semibold"
+          >
             <Image
               src="/images/logo-boryl.svg"
               alt="Company Logo"
